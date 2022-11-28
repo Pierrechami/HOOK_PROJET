26e7a818da90c0315d72827966301904c9b21282
Pierre Chaminade <pierre.chaminade.code@gmail.com>
2022-11-28 11:31:04 +0100

    initialisation projet

---
+++ oeuvre/src/main/java/mat_zai_pie/App.java
@@ -0,0 +1,13 @@
+package mat_zai_pie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
