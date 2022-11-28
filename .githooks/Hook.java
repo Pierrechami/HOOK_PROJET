982ebbd3e82edf9229ba79b14ffa78cd97a47bd5
Pierre Chaminade <pierre.chaminade.code@gmail.com>
2022-11-28 16:22:21 +0100

    Matis update hook.java

--- .githooks/Hook.java
+++ .githooks/Hook.java
@@ -5,7 +5,6 @@ import java.nio.file.Paths;
 public class Hook {
     public static void main (String[] args) throws IOException {
 
-
         String message = ""; 
         String data = "";
         // Lire le fichier COMMIT_EDITMSG qui est notre premier arguments 
@@ -13,17 +12,17 @@ public class Hook {
 
 
         // If pour savoir si le commit commence bien par Bonjour 
-        if (data.startsWith("Zainab")  data.startsWith("Matis")  data.startsWith("Pierre") ) {
-            message = "Le commit est bon, il commence bien par Bonjour ";
+        if (data.startsWith("Zainab") ||  data.startsWith("Matis") || data.startsWith("Pierre") ) {
+            message = "Le commit n'est pas bon, il ne commence pas par Matis, Zainab ou Pierre ";
             System.out.println(message);
-            System.exit(0);
+            System.exit(1); 
         } else {
             message = "Le commit ne commence pas par Zainab ou Matis ou Pierre";
             System.out.println(message);
-            System.exit(1);
+            System.exit(0);
         }
+        System.exit(1); 
 
-        System.exit(1);
     }
 
 }
