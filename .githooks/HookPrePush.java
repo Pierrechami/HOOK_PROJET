897300826d728dde8ed95707edb92baabf672173
MatisGalvin <72927745+MatisGalvin@users.noreply.github.com>
2022-12-11 17:18:43 +0100

    Matis plus d idee

--- .githooks/HookPrePush.java
+++ .githooks/HookPrePush.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class HookPrePush {
     public static void main(String[] args) throws IOException {
-        // System.out.println(args[0]);
-        System.out.println("coucou depuis le hookprepush");
+        System.out.println(args[0]);
+        // System.out.println("coucou depuis le hookprepush");
     }
 }
