239f71213f4fb4c1eb11e631d16f397a577c6e45
MatisGalvin <72927745+MatisGalvin@users.noreply.github.com>
2022-12-12 18:10:41 +0100

    Matis test push again agin agin...

--- .githooks/HookPrePush.java
+++ .githooks/HookPrePush.java
@@ -1,6 +1,6 @@
 public class HookPrePush {
     public static void main(String[] args) throws Exception {
-        System.out.println("coucou depuis le hook");
+        System.out.println("coucou depufrzis le hook");
         System.exit(1);
     }
 }
