4dd0b9f69d1ff34469faeb3b454a71a9a715ebfc
MatisGalvin <72927745+MatisGalvin@users.noreply.github.com>
2022-12-12 19:30:43 +0100

    Matis update pre push

--- .githooks/HookPrePush.java
+++ .githooks/HookPrePush.java
@@ -1,8 +1,17 @@
+import java.net.SocketTimeoutException;
+
 public class HookPrePush {
     public static void main(String[] args) throws Exception {
-        System.out.println(args[0]);
-        System.out.println(args[1]);
-        System.out.println("coucou depuis le hook");
-        System.exit(1);
+        String currentBranch = args[1];
+        String protectedBranch = args[0];
+
+        if(currentBranch.equals(protectedBranch)){
+            System.out.println("Vous etes sur une branche protegée");
+            System.exit(1);
+        }
+        else {
+            System.exit(0);
+        }
+        
     }
 }
